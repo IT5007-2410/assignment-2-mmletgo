a1feab2e577fd03426668cec359d853493d386ef
mmletgo <zhengrongh@126.com>
2024-10-04 17:42:23 +0800

    Q2

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,9 @@ class TicketToRide extends React.Component {
 
   setSelector(value)
   {
-  	/*Q2. Function to set the value of component selector variable based on user's button click.*/
+    /*Q2. Function to set the value of component selector variable based on user's button click.*/
+    this.setState({selector: value});
+    
   }
   componentDidMount() {
     this.loadData();
@@ -130,17 +132,28 @@ class TicketToRide extends React.Component {
     return (
       <div>
         <h1>Ticket To Ride</h1>
-	<div>
-	    {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
-	</div>
-	<div>
-		{/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
-		{/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
-		{/*Q3. Code to call component that Displays Travellers.*/}
-		
-		{/*Q4. Code to call the component that adds a traveller.*/}
-		{/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
-	</div>
+	      <div>
+          {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
+            <nav>
+              <button onClick={() => this.setSelector(1)}>Homepage</button>
+              <button onClick={() => this.setSelector(2)}>displayTraveller</button>
+              <button onClick={() => this.setSelector(3)}>addTraveller</button>
+              <button onClick={() => this.setSelector(4)}>deleteTraveller</button>
+            </nav>
+          
+	      </div>
+        <div>
+          {/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
+          {/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
+          {this.state.selector === 1 && <Homepage />}
+          {this.state.selector === 2 && <Display />}
+          {this.state.selector === 3 && <Add />}
+          {this.state.selector === 4 && <Delete />}
+          {/*Q3. Code to call component that Displays Travellers.*/}
+          
+          {/*Q4. Code to call the component that adds a traveller.*/}
+          {/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
+        </div>
       </div>
     );
   }
